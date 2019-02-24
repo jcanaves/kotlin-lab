0d76a1e867cf389920dd75c6a5e793e4be4fdce4
Julian Canaves <julian.canaves@gmail.com>
2019-02-24 12:19:30 +0100

    Auth

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Ktor quickstart guide
+Ktor quickstart guide: https://ktor.io/quickstart/guides/api.html
 
-https://ktor.io/quickstart/guides/api.html
+Testing RESTful Web Services: https://www.jetbrains.com/help/rider/Testing_RESTful_Web_Services.html
+
+Editor-Based HTTP client: https://www.vojtechruzicka.com/intellij-idea-tips-tricks-testing-restful-web-services/
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,11 @@
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.*
+import io.ktor.auth.Authentication
+import io.ktor.auth.UserIdPrincipal
+import io.ktor.auth.authenticate
+import io.ktor.auth.jwt.jwt
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.jackson.jackson
@@ -21,21 +27,56 @@ data class PostSnippet(val snippet: PostSnippet.Text) {
     data class Text(val text: String)
 }
 
+open class SimpleJWT(val secret: String) {
+    private val algorithm = Algorithm.HMAC256(secret)
+    val verifier = JWT.require(algorithm).build()
+    fun sign(name: String): String = JWT.create().withClaim("name", name).sign(algorithm)
+}
+
+class User(val name: String, val password: String)
+
+val users = Collections.synchronizedMap(
+    listOf(User("test", "test"))
+        .associateBy { it.name }
+        .toMutableMap()
+)
+
+class LoginRegister(val user: String, val password: String)
+
 fun Application.module() {
+    val simpleJwt = SimpleJWT("my-super-secret-for-jwt")
     install(CallLogging)
     install(ContentNegotiation) {
         jackson {
             enable(SerializationFeature.INDENT_OUTPUT) // Pretty Prints the JSON
         }
     }
-    routing {
-        get("/snippets") {
-            call.respond(mapOf("snippets" to synchronized(snippets) { snippets.toList() }))
+    install(Authentication) {
+        jwt {
+            verifier(simpleJwt.verifier)
+            validate {
+                UserIdPrincipal(it.payload.getClaim("name").asString())
+            }
         }
-        post("/snippets") {
-            val post = call.receive<PostSnippet>()
-            snippets += Snippet(post.snippet.text)
-            call.respond(mapOf("OK" to true))
+    }
+    routing {
+        post("/login") {
+            val post = call.receive<LoginRegister>()
+            val user = users.getOrPut(post.user) { User(post.user, post.password) }
+            if (user.password != post.password) error("Invalid credentials")
+            call.respond(mapOf("token" to simpleJwt.sign(user.name)))
+        }
+        route("/snippets") {
+            get {
+                call.respond(mapOf("snippets" to synchronized(snippets) { snippets.toList() }))
+            }
+            authenticate {
+                post {
+                    val post = call.receive<PostSnippet>()
+                    snippets += Snippet(post.snippet.text)
+                    call.respond(mapOf("OK" to true))
+                }
+            }
         }
     }
 }
